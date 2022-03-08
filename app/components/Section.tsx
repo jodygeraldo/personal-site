195ce57e7a2d5df6416fe48a206dcbe814f15ba7
Jody Geraldo <ajody.geraldo@gmail.com>
2022-03-09 01:48:48 +0700

    feat: add section wrapper component

---
+++ app/components/Section.tsx
@@ -0,0 +1,46 @@
+import clsx from 'clsx'
+import { ReactNode } from 'react'
+
+interface Props {
+  id: string
+  title: string
+  description?: string
+  scaleUpColor?: boolean
+  children: ReactNode
+}
+
+export default function Section({
+  children,
+  id,
+  title,
+  description,
+  scaleUpColor,
+}: Props) {
+  return (
+    <section
+      aria-labelledby={`section-${id}`}
+      className={clsx(
+        scaleUpColor && 'bg-gray-2',
+        'relative py-16 sm:py-24 lg:py-32',
+      )}
+    >
+      <div className="mx-auto max-w-md space-y-16 px-4 text-center sm:max-w-3xl sm:space-y-24 sm:px-6 lg:max-w-7xl lg:px-8">
+        <div>
+          <h2
+            id={`section-${id}`}
+            className="text-center text-3xl font-extrabold text-gray-12 sm:text-4xl lg:scroll-mt-32"
+          >
+            {title}
+          </h2>
+          {description ? (
+            <p className="mx-auto mt-5 max-w-xl text-xl text-primary-11">
+              {description}
+            </p>
+          ) : null}
+        </div>
+
+        {children}
+      </div>
+    </section>
+  )
+}
