ea94d70b5b06e88411c4502fc29486993a330434
Jody Geraldo <ajody.geraldo@gmail.com>
2022-03-08 20:37:46 +0700

    feat: add icon component

---
+++ app/components/Icon.tsx
@@ -0,0 +1,29 @@
+export type IconId =
+  | 'sun'
+  | 'moon'
+  | 'arrow-down'
+  | 'arrow-top-right'
+  | 'envelope-closed'
+  | 'github'
+  | 'twitter'
+  | 'discord'
+  | 'external-link'
+  | 'paper-plane'
+  | 'play'
+  | 'hamburger-menu'
+  | 'cross'
+
+interface Props {
+  id: IconId
+}
+
+export default function Icon({
+  id,
+  ...props
+}: Props & React.HTMLAttributes<SVGElement>) {
+  return (
+    <svg width="15" height="15" viewBox="0 0 15 15" fill="none" {...props}>
+      <use href={`/asset/icons/radix.svg#${id}`} />
+    </svg>
+  )
+}
