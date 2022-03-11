aab9153204e2c5f4f30e9326d702ef33420eb8ec
Jody Geraldo <ajody.geraldo@gmail.com>
2022-03-11 20:02:21 +0700

    refactor: move CatchBoundary for 404 error to root
    this enable to throw 404 from nested root, ex: /api/get-fact we can throw 404 from that route and it will render on root catch boundary

--- app/root.tsx
+++ app/root.tsx
@@ -1,11 +1,13 @@
 import {
   json,
+  Link,
   Links,
   LiveReload,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  useCatch,
   useLoaderData,
 } from 'remix'
 import type {
@@ -74,28 +76,11 @@ export default function App() {
   const optimisticTheme = useTheme(theme)
 
   return (
-    <html
-      lang="en"
-      className={clsx(
-        optimisticTheme === 'dark' && 'dark-theme',
-        'h-full scroll-smooth',
-      )}
-    >
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body className="h-full bg-gray-1">
-        <NotificationProvider>
-          <Outlet />
-        </NotificationProvider>
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
+    <Document theme={optimisticTheme}>
+      <NotificationProvider>
+        <Outlet />
+      </NotificationProvider>
+    </Document>
   )
 }
 
@@ -106,6 +91,81 @@ function NotificationProvider({ children }: { children: ReactNode }) {
 
       <Toast.Viewport className="fixed top-0 right-0 z-50 m-0 flex w-96 max-w-[100vw] list-none flex-col gap-10 p-6" />
     </Toast.Provider>
+  )
+}
+
+export function CatchBoundary() {
+  const caught = useCatch()
+
+  return (
+    <Document theme={caught.data?.theme}>
+      <main className="mx-auto flex min-h-full max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="flex w-full flex-grow flex-col">
+          <div className="flex-shrink-0 pt-10 sm:pt-16">
+            <Link to="/" className="flex items-center">
+              <h1 className="text-3xl font-medium tracking-tight text-primary-9">
+                Jody Geraldo
+              </h1>
+            </Link>
+          </div>
+          <div className="my-auto flex-shrink-0 py-16 sm:py-32">
+            <p className="text-sm font-semibold uppercase tracking-wide text-primary-9 lg:hidden">
+              404 error
+            </p>
+            <h1 className="mt-2 text-4xl font-extrabold tracking-tight text-gray-12 sm:text-5xl">
+              Page not found
+            </h1>
+            <p className="mt-2 text-base text-gray-11">
+              Sorry, we couldn’t find the page you’re looking for.
+            </p>
+            <div className="mt-6">
+              <Link
+                to="/"
+                className="text-base font-medium text-primary-9 hover:text-primary-10"
+              >
+                Go back home<span aria-hidden="true"> &rarr;</span>
+              </Link>
+            </div>
+          </div>
+        </div>
+        <svg
+          className="hidden self-center lg:block"
+          width="860.13137"
+          height="571.14799"
+          viewBox="0 0 860.13137 571.14799"
+        >
+          <use href="/assets/images/undraw_page_not_found.svg#page-not-found" />
+        </svg>
+      </main>
+    </Document>
+  )
+}
+
+function Document({
+  children,
+  theme = 'dark',
+}: {
+  children: ReactNode
+  theme?: 'dark' | 'light'
+}) {
+  return (
+    <html
+      lang="en"
+      className={clsx(theme === 'dark' && 'dark-theme', 'h-full scroll-smooth')}
+    >
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body className="h-full bg-gray-1">
+        {children}
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
   )
 }
 
--- app/routes/$.tsx
+++ app/routes/$.tsx
@@ -1,51 +1,11 @@
-import { Link } from 'remix'
+import { json } from 'remix'
 import type { LoaderFunction } from 'remix'
+import { getTheme } from '~/utils/theme.server'
 
-export const loader: LoaderFunction = () => {
-  throw new Response('Not Found', { status: 404 })
-}
+export const loader: LoaderFunction = async ({ request }) => {
+  const theme = await getTheme(request)
 
-export function CatchBoundary() {
-  return (
-    <main className="mx-auto flex min-h-full max-w-7xl px-4 sm:px-6 lg:px-8">
-      <div className="flex w-full flex-grow flex-col">
-        <div className="flex-shrink-0 pt-10 sm:pt-16">
-          <Link to="/" className="flex items-center">
-            <h1 className="text-3xl font-medium tracking-tight text-primary-9">
-              Jody Geraldo
-            </h1>
-          </Link>
-        </div>
-        <div className="my-auto flex-shrink-0 py-16 sm:py-32">
-          <p className="text-sm font-semibold uppercase tracking-wide text-primary-9 lg:hidden">
-            404 error
-          </p>
-          <h1 className="mt-2 text-4xl font-extrabold tracking-tight text-gray-12 sm:text-5xl">
-            Page not found
-          </h1>
-          <p className="mt-2 text-base text-gray-11">
-            Sorry, we couldn’t find the page you’re looking for.
-          </p>
-          <div className="mt-6">
-            <Link
-              to="/"
-              className="text-base font-medium text-primary-9 hover:text-primary-10"
-            >
-              Go back home<span aria-hidden="true"> &rarr;</span>
-            </Link>
-          </div>
-        </div>
-      </div>
-      <svg
-        className="hidden self-center lg:block"
-        width="860.13137"
-        height="571.14799"
-        viewBox="0 0 860.13137 571.14799"
-      >
-        <use href="/assets/images/undraw_page_not_found.svg#page-not-found" />
-      </svg>
-    </main>
-  )
+  throw json({ theme }, { status: 404 })
 }
 
 export default function NotFoundPage() {
