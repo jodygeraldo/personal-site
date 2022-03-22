f3e8c54071de5defce74f658afd94f184246b1fd
Jody Geraldo <ajody.geraldo@gmail.com>
2022-03-22 16:11:44 +0700

    refactor: move contact form to separated component

--- app/components/Contact/Contact.tsx
+++ app/components/Contact/Contact.tsx
@@ -1,41 +1,14 @@
-import { useEffect, useRef } from 'react'
-import { useFetcher } from 'remix'
-import { ActionType } from '~/routes'
-import type { ActionData as IndexActionData } from '~/routes'
 import Icon from '~/components/Icon'
-import NotificationToast from '~/components/NotificationToast'
 import type { Language, Translations } from '~/utils/i18n.server'
-import { useHydrated } from '~/hooks/useHydrated'
-import clsx from 'clsx'
+import ContactForm from './ContactForm'
 
 interface Props {
   translation: Translations['contact'][Language]
 }
 
 export default function Contact({ translation }: Props) {
-  const isHydrated = useHydrated()
-  const fetcher = useFetcher<IndexActionData>()
-
-  const ref = useRef<HTMLFormElement>(null)
-
-  const fetcherSubmiting =
-    fetcher.submission?.formData.get('action') === ActionType.SUBMIT_MESSSAGE
-
-  useEffect(() => {
-    if (fetcher.submission && fetcherSubmiting) {
-      ref.current?.reset()
-    }
-  }, [fetcher.submission, fetcherSubmiting])
-
   return (
     <section aria-labelledby="section-contact" className="relative bg-gray-1">
-      {fetcher.data && fetcher.data.statusMessage ? (
-        <NotificationToast
-          title={fetcher.data.statusMessage}
-          variant={fetcher.data.type}
-          extendedMessage={fetcher.data.extendedMessage}
-        />
-      ) : null}
       <div className="absolute inset-0">
         <div className="absolute inset-y-0 left-0 w-1/2 bg-gray-2" />
       </div>
@@ -86,116 +59,7 @@ export default function Contact({ translation }: Props) {
         </div>
         <div className="bg-gray-1 py-16 px-4 sm:px-6 lg:col-span-3 lg:py-24 lg:px-8 xl:pl-12">
           <div className="mx-auto max-w-lg lg:max-w-none">
-            <fetcher.Form
-              ref={ref}
-              method="post"
-              replace={true}
-              className="grid grid-cols-1 gap-y-6"
-            >
-              <div>
-                <label htmlFor="name" className="sr-only">
-                  {translation.name}
-                </label>
-                <input
-                  id="name"
-                  name="name"
-                  type="text"
-                  aria-invalid={
-                    fetcher.data?.fieldErrors?.name ? true : undefined
-                  }
-                  aria-describedby={
-                    fetcher.data?.fieldErrors?.name && 'name-error'
-                  }
-                  autoComplete="name"
-                  className={clsx(
-                    fetcher.data?.fieldErrors?.name &&
-                      'invalid:border-variable',
-                    'block w-full rounded-md border-gray-6 bg-gray-3 py-3 px-4 text-gray-12 placeholder-gray-9 shadow-sm focus:border-gray-7 focus:ring-gray-7',
-                  )}
-                  placeholder="Name"
-                  required={true}
-                  minLength={3}
-                  defaultValue={fetcher.data?.fields?.name}
-                />
-                {fetcher.data?.fieldErrors?.name ? (
-                  <p className="mt-2 text-sm text-variable" id="name-error">
-                    {fetcher.data.fieldErrors.name}
-                  </p>
-                ) : null}
-              </div>
-              <div>
-                <label htmlFor="email" className="sr-only">
-                  Email
-                </label>
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  aria-invalid={
-                    fetcher.data?.fieldErrors?.email ? true : undefined
-                  }
-                  aria-describedby={
-                    fetcher.data?.fieldErrors?.email && 'email-error'
-                  }
-                  autoComplete="email"
-                  className={clsx(
-                    fetcher.data?.fieldErrors?.email &&
-                      'invalid:border-variable',
-                    'block w-full rounded-md border-gray-6 bg-gray-3 py-3 px-4 text-gray-12 placeholder-gray-9 shadow-sm focus:border-gray-7 focus:ring-gray-7',
-                  )}
-                  placeholder="Email"
-                  required={true}
-                  defaultValue={fetcher.data?.fields?.email}
-                />
-                {fetcher.data?.fieldErrors?.email ? (
-                  <p className="mt-2 text-sm text-variable" id="email-error">
-                    {fetcher.data.fieldErrors.email}
-                  </p>
-                ) : null}
-              </div>
-              <div>
-                <label htmlFor="message" className="sr-only">
-                  {translation.message}
-                </label>
-                <textarea
-                  id="message"
-                  name="message"
-                  aria-invalid={
-                    fetcher.data?.fieldErrors?.message ? true : undefined
-                  }
-                  aria-describedby={
-                    fetcher.data?.fieldErrors?.message && 'message-error'
-                  }
-                  rows={4}
-                  className={clsx(
-                    fetcher.data?.fieldErrors?.message &&
-                      'invalid:border-variable',
-                    'block w-full rounded-md border-gray-6 bg-gray-3 py-3 px-4 text-gray-12 placeholder-gray-9 shadow-sm focus:border-gray-7 focus:ring-gray-7',
-                  )}
-                  placeholder="Message"
-                  required={true}
-                  minLength={20}
-                  defaultValue={fetcher.data?.fields?.message}
-                />
-                {fetcher.data?.fieldErrors?.message ? (
-                  <p className="mt-2 text-sm text-variable" id="message-error">
-                    {fetcher.data.fieldErrors.message}
-                  </p>
-                ) : null}
-              </div>
-              <div>
-                <button
-                  name="action"
-                  value={ActionType.SUBMIT_MESSSAGE}
-                  className="inline-flex justify-center rounded-md border border-transparent bg-gray-3 py-3 px-6 text-base font-medium text-gray-11 shadow-sm hover:bg-gray-4 focus:outline-none focus:ring-2 focus:ring-gray-7 focus:ring-offset-2 focus:ring-offset-gray-1 active:bg-gray-5 disabled:cursor-not-allowed disabled:opacity-50"
-                  disabled={!isHydrated || fetcherSubmiting}
-                >
-                  {fetcherSubmiting
-                    ? translation.buttonSubmitting
-                    : translation.button}
-                </button>
-              </div>
-            </fetcher.Form>
+            <ContactForm translation={translation} />
           </div>
         </div>
       </div>
