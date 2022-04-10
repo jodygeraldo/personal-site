b00aec6a1900f50f330d0f280e05760ea65dd470
Jody Geraldo <ajody.geraldo@gmail.com>
2022-04-10 22:24:47 +0700

    Refactor: use radix popover instead of headlessui (#11)
    * add radix popover
    
    * simplify setence
    
    * refactor headlessui popover to radix popover
    
    * fix: wrong close button position

--- app/components/Hero/HeroHeader/HeroHeader.tsx
+++ app/components/Hero/HeroHeader/HeroHeader.tsx
@@ -1,47 +1,89 @@
-import { Popover } from '@headlessui/react'
+import * as Popover from '@radix-ui/react-popover'
 import { Link } from '@remix-run/react'
 import Icon from '~/components/Icon'
+import LanguageSwitcher from '~/components/LanguageSwitcher'
 import type { Language, Translations } from '~/utils/i18n.server'
 import HeroHeaderNav from './HeroHeaderNav'
-import HeroHeaderNavMobile from './HeroHeaderNavMobile'
 
 interface Props {
   translation: Translations['heroHeader'][Language]
   language: Language
 }
 
-export default function HeroHeader({ translation, language }: Props) {
+export default function RadixHeroHeader({ translation, language }: Props) {
   return (
-    <Popover className="flex h-16 justify-between py-16">
-      {({ close }) => (
-        <>
-          <div className="flex w-full items-center justify-between px-2 lg:px-0">
-            <Link to="/" className="flex items-center">
-              <h1 className="text-3xl font-medium tracking-tight text-primary-9">
-                Jody Geraldo
-              </h1>
-            </Link>
-            <HeroHeaderNav translation={translation} language={language} />
-          </div>
-          <div className="flex items-center lg:hidden">
-            {/* Mobile menu button */}
-            <Popover.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-11 hover:bg-gray-2 hover:text-gray-12 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-7">
-              <span className="sr-only">{translation['open-menu']}</span>
-              <Icon
-                id="hamburger-menu"
-                className="block h-6 w-6 text-gray-11 group-hover:text-gray-12"
-                aria-hidden="true"
-              />
-            </Popover.Button>
-          </div>
+    <Popover.Root modal={true}>
+      <div className="flex h-16 justify-between py-16">
+        <div className="flex w-full items-center justify-between px-2 lg:px-0">
+          <Link to="/" className="flex items-center">
+            <h1 className="text-3xl font-medium tracking-tight text-primary-9">
+              Jody Geraldo
+            </h1>
+          </Link>
+          <HeroHeaderNav translation={translation} language={language} />
+        </div>
+        <div className="flex items-center lg:hidden">
+          <Popover.Trigger className="inline-flex items-center justify-center rounded-md p-2 text-gray-11 hover:bg-gray-2 hover:text-gray-12 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-7 state-open:hidden">
+            <span className="sr-only">{translation['open-menu']}</span>
 
-          <HeroHeaderNavMobile
-            translation={translation}
-            setClose={close}
-            language={language}
-          />
-        </>
-      )}
-    </Popover>
+            <Icon
+              id="hamburger-menu"
+              className="block h-6 w-6 text-gray-11 group-hover:text-gray-12"
+              aria-hidden={true}
+            />
+          </Popover.Trigger>
+        </div>
+      </div>
+
+      <Popover.Anchor className="absolute top-0 left-0 right-0" />
+      <Popover.Content>
+        <div className="relative mt-2 min-w-[95vw] divide-y divide-gray-6 rounded-lg bg-gray-2 shadow-lg ring-1 ring-gray-7 ring-opacity-5 sm:min-w-[97.5vw]">
+          <Popover.Close className="absolute right-2 top-2 inline-flex items-center justify-center rounded-md p-2 text-gray-11 hover:bg-gray-3 hover:text-gray-12 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-7">
+            <span className="sr-only">{translation['close-menu']}</span>
+            <Icon
+              id="cross"
+              className="block h-6 w-6 text-gray-11 group-hover:text-gray-12"
+              aria-hidden="true"
+            />
+          </Popover.Close>
+
+          <div className="pt-3 pb-2">
+            <div className="flex items-center justify-between px-4">
+              <Link to="/">
+                <h2 className="text-3xl font-medium tracking-tight text-primary-9">
+                  Jody Geraldo
+                </h2>
+              </Link>
+            </div>
+            <div className="mt-3 space-y-1 px-2">
+              <Link
+                className="block w-full rounded-md py-2 px-3 text-lg font-medium text-gray-11 hover:bg-gray-3 hover:text-gray-12"
+                to="/#section-contact"
+              >
+                {translation['contact']}
+              </Link>
+              <Link
+                prefetch="intent"
+                className="block w-full rounded-md py-2 px-3 text-lg font-medium text-gray-11 hover:bg-gray-3 hover:text-gray-12"
+                to="/resume"
+              >
+                Resume
+              </Link>
+              <a
+                className="block w-full rounded-md py-2 px-3 text-lg font-medium text-gray-11 hover:bg-gray-3 hover:text-gray-12"
+                href="https://github.com/jodygeraldo/personal-site"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                {translation['source-code']}
+              </a>
+            </div>
+          </div>
+          <div className="space-y-1 p-2">
+            <LanguageSwitcher translation={translation} language={language} />
+          </div>
+        </div>
+      </Popover.Content>
+    </Popover.Root>
   )
 }
