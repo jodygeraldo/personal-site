6448fab09bf6c8fa8a139f76a771608111cd8fc8
Jody Geraldo <ajody.geraldo@gmail.com>
2022-04-12 17:19:19 +0700

    add title on error page

--- app/routes/$.tsx
+++ app/routes/$.tsx
@@ -1,7 +1,11 @@
-import type { LoaderFunction } from '@remix-run/cloudflare'
+import type { LoaderFunction, MetaFunction } from '@remix-run/cloudflare'
 import { json } from '@remix-run/cloudflare'
 import { getLanguage, getTranslations } from '~/utils/i18n.server'
 import { getTheme } from '~/utils/theme.server'
+
+export const meta: MetaFunction = () => ({
+  title: 'Whoops...',
+})
 
 export const loader: LoaderFunction = async ({ request }) => {
   const language = await getLanguage(request)
