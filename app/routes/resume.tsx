f32093a1dd7cc66b505c556d3e5421a3b875ee0b
Jody Geraldo <ajody.geraldo@gmail.com>
2022-04-13 09:58:39 +0700

    compress location marker icon

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -90,17 +90,17 @@ export default function ResumePage() {
                         fill="none"
                         viewBox="0 0 24 24"
                         stroke="currentColor"
-                        strokeWidth={1.5}
+                        stroke-width={1.5}
                       >
                         <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M17.657 16.657 13.414 20.9a1.998 1.998 0 0 1-2.827 0l-4.244-4.243a8 8 0 1 1 11.314 0z"
                         />
                         <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M15 11a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"
                         />
                       </svg>
                     ) : null}
