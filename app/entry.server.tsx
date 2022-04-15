8bb858568a9ffc6a5a506c292faf6b7082c59cf3
Jody Geraldo <ajody.geraldo@gmail.com>
2022-04-15 14:47:34 +0700

    enable react 18 streaming

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,21 +1,41 @@
 import type { EntryContext } from '@remix-run/cloudflare'
 import { RemixServer } from '@remix-run/react'
-import { renderToString } from 'react-dom/server'
+import isbot from 'isbot'
+import { renderToReadableStream } from 'react-dom/server'
 
-export default function handleRequest(
+export default async function handleRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
   remixContext: EntryContext,
 ) {
-  const markup = renderToString(
-    <RemixServer context={remixContext} url={request.url} />,
-  )
+  let didError = false
 
-  responseHeaders.set('Content-Type', 'text/html')
+  try {
+    let stream = await renderToReadableStream(
+      <RemixServer context={remixContext} url={request.url} />,
+      {
+        onError(error) {
+          didError = true
+          console.error(error)
+        },
+      },
+    )
 
-  return new Response('<!DOCTYPE html>' + markup, {
-    status: responseStatusCode,
-    headers: responseHeaders,
-  })
+    if (isbot(request.headers.get('user-agent'))) {
+      await stream.allReady
+    }
+
+    responseHeaders.set('Content-Type', 'text/html')
+
+    return new Response(stream, {
+      status: didError ? 500 : responseStatusCode,
+      headers: responseHeaders,
+    })
+  } catch (error) {
+    return new Response('<!DOCTYPE html><p>Loading...</p>', {
+      status: responseStatusCode,
+      headers: responseHeaders,
+    })
+  }
 }
